9bb3979ad3a80072e198e9026c6fff5d399b0ef8
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-06-26 14:47:50 -0300

    Feat: Dark mode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finansas-dashboard';
+  isDarkMode = false;
+  ngOnInit() {
+    this.configDarkMode();
+    this.updateDarkModeClass();
+  }
+
+  configDarkMode() {
+    // Detecta si el sistema está en modo oscuro
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Si el usuario ya eligió un modo antes, respetalo
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+      this.isDarkMode = savedTheme === 'dark';
+    } else {
+      this.isDarkMode = prefersDark;
+    }
+  }
+
+  updateDarkModeClass(): void {
+    const html = document.documentElement;
+    if (this.isDarkMode) {
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
+  }
+
 }
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,26 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isDarkMode = false;
   @Output() openModal = new EventEmitter<void>();
+
+  ngOnInit() {
+    if(localStorage.getItem('theme') === 'dark') {
+      this.isDarkMode = true;
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const htmlElement = document.documentElement;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+
+    if (this.isDarkMode) {
+      htmlElement.classList.add('dark');
+    } else {
+      htmlElement.classList.remove('dark');
+    }
+  }
+
 }
